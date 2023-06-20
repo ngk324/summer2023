706c07124a61efa8d6841ff0aa2d553a4bc4a2a4
ngk324 <89586451+ngk324@users.noreply.github.com>
2023-06-20 15:19:47 -0400

    Add files via upload

--- net-dyn-2nd-develop/src/main.cpp
+++ net-dyn-2nd-develop/src/main.cpp
@@ -13,6 +13,24 @@
 
 #include <cmath>
 
+#include <numeric>
+
+/*double compute_var_grad(const std::vector<double>& values, double mean, Eigen::Matrix<float, -1, 1> vki, Eigen::Matrix<float, -1, 1> vkj){
+    double d_var = 0; 
+    for(int i = 0; i < values.size(); i++){
+        d_var = d_var + (2.0/values.size()) * (values[i]-mean)*(vki[i] * vki[i] + vkj[i]*vkj[i] - 2.0*vki[i]*vkj[i]);
+    }
+    return d_var;
+}*/
+
+double compute_var_grad(const std::vector<double>& values, double mean, Eigen::Matrix<float, -1, 1> vki, Eigen::Matrix<float, -1, 1> vkj){
+    double d_var = 0; 
+    for(int i = 0; i < values.size(); i++){
+        d_var = d_var + (2.0/values.size()) * (values[i]-mean)*(vki[i] * vki[i] + vkj[i]*vkj[i] - 2.0*vki[i]*vkj[i]);
+    }
+    return d_var;
+}
+
 double round(double var, int place)
 {
     double value = (int)(var * 100 + .5);
@@ -100,7 +118,7 @@ void plotHeatMap(Eigen::Matrix<float, -1, 1> arr, int size)
 }
 
 
-void plotHistogram(const std::vector<double>& values, int bins = 9)
+void plotHistogram(const std::vector<double>& values, int bins = 8)
 {
     // Find the minimum and maximum values in the vector
     int minValue = *std::min_element(values.begin(), values.end());
@@ -159,6 +177,32 @@ std::vector<std::pair<float, Eigen::VectorXf>> get_eigen_pairs(Eigen::MatrixXf &
     return eigen_pairs;
 }
 
+std::vector<std::pair<float, Eigen::VectorXf>> get_eigen_pairs2(Eigen::MatrixXf &matrix)
+{
+    std::vector<std::pair<float, Eigen::VectorXf>> eigen_pairs;
+    //Eigen::SelfAdjointEigenSolver<Eigen::MatrixXf> solver(matrix);
+    Eigen::EigenSolver<Eigen::MatrixXf> solver(matrix);
+    Eigen::VectorXcf eigVals = solver.eigenvalues();
+    Eigen::MatrixXcf eigVecs = solver.eigenvectors();
+
+    //eigVecs = eigVecs.transpose();
+
+    for (int i{0}; i < eigVals.size(); i++)
+    {
+        /*if (eigVals(i).imag() != 0)
+        {
+            std::cout << "Complex eigenvalue!" << std::endl;
+            exit(EXIT_FAILURE);
+        }*/
+        eigen_pairs.push_back(std::make_pair(eigVals(i).real(), eigVecs.row(i).real()));
+    }
+    std::sort(eigen_pairs.begin(), eigen_pairs.end(), [&](const std::pair<float, Eigen::VectorXf> &a, const std::pair<float, Eigen::VectorXf> &b) -> bool
+              { return a.first < b.first; });
+    return eigen_pairs;
+}
+
+
+
 int findModeIndex(const std::vector<double>& nums) {
     std::unordered_map<double, double> freq;
     int maxFreq = 0;
@@ -172,7 +216,7 @@ int findModeIndex(const std::vector<double>& nums) {
             modeIndex = i;
         }
     }
-    std::cout << "FreqCount: " << maxFreq << "\n";
+    //std::cout << "FreqCount: " << maxFreq << "\n";
     return modeIndex;
 }
 
@@ -184,23 +228,25 @@ int main(int argc, char *argv[])
         decentralizedAlg = std::stoi(argv[1]);
     }
     // Initialize and construct simple graph
-    int xGrid{2}, yGrid{2};
+    int xGrid{5}, yGrid{5};
     Graph my_graph;
     my_graph.constructSimpleGraph(xGrid, yGrid);
     my_graph.computeMatrices();
 
-    int MAX_X = 2;
-    int MAX_Y = 2;
+    int MAX_X = 5;
+    int MAX_Y = 5;
     int PLOT_SCALE = 70;
     int vPad = 2;
     int hPad = 2;
 
+    /*
     for(int i = 0; i < MAX_X*MAX_X; i++){
         for(int j = 0; j < MAX_Y*MAX_Y; j++){
             std::cout << i << ", " << j << ": " << my_graph.laplacianMatrix(i,j) << "\n";
         }
-    }
-
+    }*/
+    
+    std::cout << "\n\n";
     double damping{0.1}, stiffness{5}, epsilon{0.01};
 
     double amp{1};
@@ -211,24 +257,91 @@ int main(int argc, char *argv[])
     float chosenEigVal{0};
     Eigen::VectorXf chosenEigVec;
 
-    std::cout << "end eign comp." << std::endl;
+    //std::cout << "end eign comp." << std::endl;
 
     std::vector<double> ev;
     for (int i{0}; i < eigen_pairs.size(); i++)
     {
         ev.push_back(round(eigen_pairs[i].first,2));
-        std::cout << i << ": " << ev[i] << "\n";
+        //std::cout << i << ": " << ev[i] << "\n";
     }
 
     int modeIndex = findModeIndex(ev);
 
-    calc_steady_state(ev, ev[modeIndex]);
+    //calc_steady_state(ev, ev[modeIndex]);
 
+    Eigen::MatrixXf var_grad(MAX_X, MAX_Y);
+    std::vector<double> var_gradient;
+
+    double mean = std::accumulate(ev.begin(), ev.end(), 0.0) / ev.size();
+
+    /*
+    std::cout  << "Size 1: " << my_graph.nodes.size() << "\n";
+    std::cout  << "Size 2: " << my_graph.nodes[0]->neighbors.size() << "\n";
+    std::cout  << "Value 1: " << (*my_graph.nodes[0]).id << "\n";
+    std::cout  << "Value 2: " << (*my_graph.nodes[0]->neighbors[1]).id << "\n";
+    */
+
+    auto eigen_pairs3 = get_eigen_pairs2(B_Matrix);
+    /*
+    std::cout << "VECTORS \n\n";
+
+    for(int x = 0; x < eigen_pairs3[0].second.size(); x++){
+        std::cout << "Vector " << x << " " << eigen_pairs3[x].second << "\n";
+    }
+    */
+    //Eigen::MatrixXf test =
+
+/*
+    int i = 3;
+    int j = 2;
+    for(int x = 0; x < eigen_pairs[0].size(); x++){
+        for(int y = 0; y < eigen_pairs.size(); y++){
+
+        }
+    }*/
+
+    //std::cout << "test " << eigen_pairs[].second.row(0) << "\n\n";
+
+    for(int i = 0; i < my_graph.nodes.size(); i++){
+        for(int j = 0; j < (my_graph.nodes[i])->neighbors.size(); j++){
+            /*if(my_graph.nodes[i]->isNeighbor(my_graph.nodes[j])){
+                var_grad(i,j) = compute_var_grad(ev, mean, eigen_pairs[(*my_graph.nodes[0]).id].second, eigen_pairs[(*my_graph.nodes[0]->neighbors[1]).id].second);
+            }*/
+            if((*my_graph.nodes[i]).id < (*my_graph.nodes[i]->neighbors[j]).id){
+                var_gradient.push_back(compute_var_grad(ev, mean, eigen_pairs3[(*my_graph.nodes[i]).id].second, eigen_pairs3[(*my_graph.nodes[i]->neighbors[j]).id].second));
+                //std::cout << (*my_graph.nodes[i]).id << ", " << (*my_graph.nodes[i]->neighbors[j]).id << ": " << (compute_var_grad(ev, mean, eigen_pairs[(*my_graph.nodes[i]).id].second, eigen_pairs[(*my_graph.nodes[i]->neighbors[j]).id].second)) << "\n";
+                //var_gradient.push_back(compute_var_grad(ev, mean, eigen_pairs[].second, eigen_pairs[].second));
+            }
+        }
+    }
+
+    /*for(int i = 0; i < var_gradient.size(); i++){
+        std::cout << i << ": " << var_gradient[i] << "\n";
+    }*/
+
+    double vec_dot_unit_norm = 0;
+    std::vector<double> dot_vec;
+
+    for(int i = 0; i < var_gradient.size(); i++){
+        vec_dot_unit_norm = vec_dot_unit_norm + (var_gradient[i] * (1.0/sqrt(var_gradient.size())));
+    }
+
+    for(int i = 0; i < var_gradient.size(); i++){
+        dot_vec.push_back(var_gradient[i] - (vec_dot_unit_norm * (1.0/sqrt(var_gradient.size()))));
+    }
+    
+    for(int i = 0; i < dot_vec.size(); i++){
+        std::cout << i << ": " << dot_vec[i] << "\n";
+    }
+    std::cout << "\n\n";
+
+    /*
     if (modeIndex != -1) {
         std::cout << "Mode Index: " << modeIndex << " Mode Value: " << ev[modeIndex] << std::endl;
     } else {
         std::cout << "No mode found." << std::endl;
-    }
+    }*/
 
     chosenEigVal = eigen_pairs[modeIndex].first;
 
@@ -238,9 +351,54 @@ int main(int argc, char *argv[])
     
     
     // Generate plot
+    Plot my_plot2("State Plot - Chosen EigVal: " + std::to_string(chosenEigVal), PLOT_SCALE, vPad, hPad, MAX_X, MAX_Y);
+    my_plot2.plotGraph(my_graph);
+    my_plot2.displayPlot(true);
+
+    int counter = 0;
+    for(int i = 0; i < my_graph.nodes.size(); i++){
+        for(int j = 0; j < (my_graph.nodes[i])->neighbors.size(); j++){
+            /*if(my_graph.nodes[i]->isNeighbor(my_graph.nodes[j])){
+                var_grad(i,j) = compute_var_grad(ev, mean, eigen_pairs[(*my_graph.nodes[0]).id].second, eigen_pairs[(*my_graph.nodes[0]->neighbors[1]).id].second);
+            }*/
+            if((*my_graph.nodes[i]).id < (*my_graph.nodes[i]->neighbors[j]).id){
+                my_graph.adjacencyMatrix((*my_graph.nodes[i]).id, (*my_graph.nodes[i]->neighbors[j]).id) = my_graph.adjacencyMatrix((*my_graph.nodes[i]).id, (*my_graph.nodes[i]->neighbors[j]).id) + dot_vec[counter];
+                my_graph.adjacencyMatrix((*my_graph.nodes[i]->neighbors[j]).id, (*my_graph.nodes[i]).id) = my_graph.adjacencyMatrix((*my_graph.nodes[i]->neighbors[j]).id, (*my_graph.nodes[i]).id) + dot_vec[counter];
+                //std::cout << (*my_graph.nodes[i]).id << ", " << (*my_graph.nodes[i]->neighbors[j]).id << ": " << (compute_var_grad(ev, mean, eigen_pairs[(*my_graph.nodes[i]).id].second, eigen_pairs[(*my_graph.nodes[i]->neighbors[j]).id].second)) << "\n";
+                std::cout << ((*my_graph.nodes[i]).id) << ", " << (*my_graph.nodes[i]->neighbors[j]).id << " - " << dot_vec[counter] << "\n";
+                counter++;
+            }
+        }
+    }
+
+    std::cout << "\n\n";
+
+    my_graph.computeMatrices2();
+    Eigen::MatrixXf B_Matrix2 = my_graph.laplacianMatrix;
+    auto eigen_pairs2 = get_eigen_pairs(B_Matrix2);
+
+    std::vector<double> ev2;
+    for (int i{0}; i < eigen_pairs2.size(); i++)
+    {
+        ev2.push_back(round(eigen_pairs2[i].first,2));
+        //std::cout << i << ": " << ev[i] << "\n";
+    }
+
+    for(int i = 0; i < MAX_X*MAX_X; i++){
+        for(int j = 0; j < MAX_Y*MAX_Y; j++){
+            std::cout << i << ", " << j << ": " << my_graph.laplacianMatrix(i,j) << "\n";
+        }
+    }
+
+    modeIndex = findModeIndex(ev2);
+
+    plotHistogram(ev2);
+
     Plot my_plot("State Plot - Chosen EigVal: " + std::to_string(chosenEigVal), PLOT_SCALE, vPad, hPad, MAX_X, MAX_Y);
     my_plot.plotGraph(my_graph);
     my_plot.displayPlot(true);
+
+    plotHeatMap(eigen_pairs2[modeIndex].second, MAX_X);
     
     /*
     double freq{sqrt(chosenEigVal)};
